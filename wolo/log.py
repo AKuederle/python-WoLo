173d40eee83b3e65beab67aa519726765d34358b
Arne Kuederle <a.kuederle@gmail.com>
2016-05-31 00:07:11 +0200

    More Documentation

--- wolo/log.py
+++ wolo/log.py
@@ -5,6 +5,24 @@ from .helper import pretty_print_index, convert_return
 
 
 class TaskLog():
+    """Object that holds all information about a single Task-clas instance. This
+    class is not ment to be used by the user, but is only used internally.
+
+    For listing and user documentation about the available Task properties refer
+    to wolo.log.FlatView.cols.
+
+    Implimentation Notes:
+    - Properties can be accssesed using the . or []. The getitem method ([])
+      allows for selcting multiple properties at once when a list is passed.
+    - Getitem returns a key-value dictionary and not just the values
+    - Iterating returns key-value pairs of the properties
+    - The representation is similar to the one of itertools.namedtuple
+    - _to_dict does NOT return just a dict of the key-value pairs. It returns a
+      string representation of the index AND the dict.
+    - The class itself has a method (_from_dict) to generate a new TaskLog
+      instance from a dictionary. This is used, when log information is loaded
+      from json.
+     """
     def __init__(self, index, task_class, inputs={}, outputs={}, info={}, last_run_success=None, last_run=None, execution_time=None):
         self.index = index
         self.task_class = task_class
@@ -33,12 +51,15 @@ class TaskLog():
         return not self.__eq__(other)
 
     def _to_dict(self):
+        """return a string representation of the index and a key-value dict of
+        the properties"""
         i = pretty_print_index(self.index, style="underscore")
         value = dict(self)
         return i, value
 
     @classmethod
     def _from_dict(cls, dict):
+        """Create a new TaskLog instance from a property dictionary. This is a classmethod."""
         return cls(**dict)
 
 
@@ -90,19 +111,25 @@ class Log():
 
 
 class FlatView():
-    """FlatView is a flat Dictionary representation of a Log. The intendet usage is to select wanted columns and then exporting it for external Dataanalysis.
-    The selectable columns using the .cols(selection) methode are:
+    """A flat Dictionary representation of a Log. Flat means that the Log is not
+    nested (compare in the tree representation) The intendet usecase is to
+    select wanted columns and then exporting it for external Dataanalysis. The
+    selectable columns using the .cols(selection) methode are:
     - "task_class": Class/Name of the task
     - "inputs": Dict containing all inputs
     - "outputs": Dict containing all outputs
     - "info": Dict containing addtional information returnend by the after method
     - "last_run_success": True or False depending if the last time the run was successful
     - "index": index in tuple form. A string representation of index is already used as main object identifier
+    - "execution_time": Execution time of the task's run method, measured during the last rerunning
+    - "last_run": Last date the Task the task ran
 
-    Note: The col methods still passes all the information to the new Object. So the col selection can be changed from the same Object.
+    Note: The col methods still passes all the information to the new Object. So
+    the col selection can be changed from the same Object.
 
-    It is also possible to create a new column from a specific input or output using the .col_from_prop(prop, subprop) method using "inputs"
-    or "outputs" as prop and the wanted parameter name as subprop.
+    It is also possible to create a new column from a specific input or output
+    using the .col_from_prop(prop, subprop) method using "inputs" or "outputs"
+    as prop and the wanted parameter name as subprop.
     """
 
     def __init__(self, log, initial=None, flatten=True):
